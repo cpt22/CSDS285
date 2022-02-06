164a65a99e913866b2884b1c3dcd8da9b6bf09b4
Christian Tingle <ctingle18@gmail.com>
2022-02-06 17:21:12 -0500

    Two features working

---
+++ Project1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Project 1 - cpt15</title>
+</head>
+<body>
+    <div style="border: 1px solid black; padding: 5px">
+        <form method="get" action="load_data.php">
+            <label for="url_in">Enter a URL:</label>
+            <input id="url_in" type="text" name="url_in">
+            <button type="submit" name="tag_count" id="url_submit">Count Tags</button>
+            <button type="submit" name="resize_image" id="url_submit">Resize Images</button>
+        </form>
+    </div>
+</body>
+</html>
